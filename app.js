554b6ae330962f6db9e54c75736642a1aa68b71e
Eliasc <elicamillozzi@gmail.com>
2023-05-27 00:28:12 -0300

    tercer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+'use strict';
+
+const grande = document.querySelector('.grande');
+const punto = document.querySelectorAll('.punto');
+
+punto.forEach((cadaPunto, i) => {
+    punto[i].addEventListener('click', () => {
+        let posicion = i;
+        let operacion = posicion * -33.333;
+
+        grande.style.transform = `translateX(${operacion}%)`;
+
+        punto.forEach((cadaPunto, i) => {
+            punto[i].classList.remove('activo');
+        });
+        punto[i].classList.add('activo');
+    });
+});
